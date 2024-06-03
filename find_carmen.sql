130b9c0a218f919424a2b19cc1f71703ca404539
malikmai <malikcreese@gmail.com>
2024-06-03 16:20:56 -0400

    CARMEN SANDIEGO - Found her.

--- find_carmen.sql
+++ find_carmen.sql
@@ -49,7 +49,6 @@ WHERE code IN (
     AND percentage = 100
 );
 
-
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 SELECT name 
@@ -57,15 +56,25 @@ FROM city
 WHERE countrycode = (SELECT code FROM country WHERE name = 'Vatican City') 
   AND name <> 'Vatican City';
 
-
-
-
-
-
-
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+SELECT name 
+FROM city 
+WHERE name LIKE 'Vat%' 
+  AND name != 'Vatican City' 
+  AND countrycode IN (
+    SELECT code 
+    FROM country 
+    WHERE continent = 'South America'
+);
+
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
+
+SELECT name 
+FROM city 
+WHERE id = (SELECT capital 
+            FROM country 
+            WHERE code = (SELECT countrycode FROM city WHERE name = 'Vaticano'));
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
 
@@ -77,3 +86,9 @@ WHERE countrycode = (SELECT code FROM country WHERE name = 'Vatican City')
 -- I need a little more sunshine with my slice of life.
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
+
+SELECT name 
+FROM city 
+WHERE population = 91085;
+
+-- Carmen's final location is Encarnación, Paraguay
